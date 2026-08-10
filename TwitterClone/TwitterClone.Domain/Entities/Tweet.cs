57aeaeb3fefa44cdbeb49c1987c9db1778436c75
nazmulh0001 <nazmulhasangub001@gmail.com>
2026-08-10 22:26:15 +0600

    implemented tweet and user entities

--- TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace TwitterClone.Domain.Entities
 {
-    internal class Tweet
+    public class Tweet
     {
+        private Guid _id;
+        private Guid _authorId;
+        private string _content;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+        public Guid AuthorId
+        {
+            get { return _authorId; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
     }
 }

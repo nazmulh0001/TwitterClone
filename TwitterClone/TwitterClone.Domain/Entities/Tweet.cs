cf8206e8cc15e3c7d13abc5d58d240c09ad48ff0
nazmulh0001 <nazmulhasangub001@gmail.com>
2026-08-11 22:16:55 +0600

    added tweet.cs

--- TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -6,13 +6,44 @@ namespace TwitterClone.Domain.Entities
 {
     public class Tweet
     {
-        public string Content { get; set; }
-        public User Author { get; set; }
+        private Guid _id;
+        private Guid _userId;
+        private string _content;
+        private DateTime _createdAt;
+        private DateTime _modifiedAt;
 
-        public Tweet(string content, User author)
+        public Tweet()
         {
-            Content = content;
-            Author = author;
+            _id = Guid.NewGuid();
+            _createdAt = DateTime.UtcNow;
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+        }
+
+        public DateTime ModifiedAt
+        {
+            get { return _modifiedAt; }
+            set { _modifiedAt = value; }
         }
     }
 }
